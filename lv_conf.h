1775e0e3c2121b04906cc7e500fff4cffc05c488
Suhaas Joshi <s-joshi@ti.com>
2025-02-05 14:17:56 +0530

    lv_conf: Change depreciated macro name
    Building the demo gives a warning stating that
    LV_FS_DEFAULT_DRIVE_LETTER is depreciated. The new name for the macro is
    LV_FS_DEFAULT_DRIVER_LETTER. Hence make this change.
    
    Signed-off-by: Suhaas Joshi <s-joshi@ti.com>

--- lv_conf.h
+++ lv_conf.h
@@ -698,7 +698,7 @@
 /*File system interfaces for common APIs */
 
 /*Setting a default driver letter allows skipping the driver prefix in filepaths*/
-#define LV_FS_DEFAULT_DRIVE_LETTER 'A'
+#define LV_FS_DEFAULT_DRIVER_LETTER 'A'
 
 /*API for fopen, fread, etc*/
 #define LV_USE_FS_STDIO 1
